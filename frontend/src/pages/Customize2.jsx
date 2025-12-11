52a7a633ed6532d384bfac778e110b40a742992c
Dhiraj Patil <155843506+Dhiraj110706@users.noreply.github.com>
2025-12-11 22:00:25 +0530

    Fix import path for userDataContext

--- frontend/src/pages/Customize2.jsx
+++ frontend/src/pages/Customize2.jsx
@@ -2,7 +2,7 @@
 import React,{useContext, useState} from 'react'
 import { useNavigate } from 'react-router-dom'
 // import UserContext from '../context/userContext';
-import { userDataContext } from '../context/userContext.jsx';
+import { userDataContext } from '../context/UserContext.jsx';
 import axios from 'axios';
 import { MdKeyboardBackspace } from "react-icons/md";
 
