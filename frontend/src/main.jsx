b02d9d7058d3c6f40c4d78a455bcb176602612f4
Dhiraj Patil <155843506+Dhiraj110706@users.noreply.github.com>
2025-12-11 21:56:42 +0530

    Fix import case for UserContext component

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 // import User from '../../backend/models/user.model.js'
-import UserContext from './context/userContext.jsx'
+import UserContext from './context/UserContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
